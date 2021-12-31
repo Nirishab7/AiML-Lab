448faa20ed2f70a17765c415e6b874b031205a8b
Nirishab7 <nirishab7@gmail.com>
2021-12-31 18:50:26 +0530

    modified

--- a star/astar.py
+++ a star/astar.py
@@ -30,13 +30,13 @@ class astar:
         self.path = path
      
      # A* Algorithm
-    def astarfun(self,arr,new_item):
+    def astarfun(self,arr):
         while arr:
             cur_item = arr[0]
             cur_node = cur_item.path[-1]
             cur_cost = cur_item.cost
             cur_path = cur_item.path
-            for i in range(0,len(h)):
+            for i in range(len(h)):
                 if g[cur_node][i]!=inf and g[cur_node][i]!=0:
                     new_cost = cur_cost - h[cur_node] + h[i] + g[cur_node][i]
                     new_path = cur_path.copy()
@@ -54,5 +54,5 @@ arr = []
 
 new_item = astar(h[src],[src])
 arr.append(new_item)
-new_item.astarfun(arr,new_item)
+new_item.astarfun(arr)
 
